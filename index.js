6f48f15dda44268092caf3255ee0d724a44818b4
akashislam1 <m.naimul110@gmail.com>
2023-05-01 21:27:54 +0600

    added chefData on server

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 
+const chefData = require("./data/chef.json");
+
 app.use(cors());
+
+app.get("/chefdata", (req, res) => {
+  res.send(chefData);
+});
 
 app.get("/", (req, res) => {
   res.send("The Pasta People  Server!");
